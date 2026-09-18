941e82861d6284ab5e13ac85744f64e34b2a280a
Erwindi <110979153+ErwinWork21@users.noreply.github.com>
2026-09-18 18:09:06 +0700

    update error on signup

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -80,7 +80,7 @@ export default function LoginPage() {
             <LoginForm />
           </Suspense>
           <div className="mt-4 text-center text-sm">
-            Don't have an account?{' '}
+            Don&apos;t have an account?{' '}
             <a href="/signup" className="underline hover:text-primary">
               Sign up
             </a>
